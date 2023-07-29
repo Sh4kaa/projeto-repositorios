9d2f3fcdfa4d837bb477dc4c36c2ba9634b1d056
Dario de souza <darioo_dario@hotmail.com>
2023-07-29 12:27:56 -0300

    buscando repos

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,15 +1,40 @@
 import { FaGithub, FaPlus } from "react-icons/fa";
 import { Container, Form, SubmitButton } from "./styles";
+import { useCallback, useState } from "react";
+
+import api from "../../services/api";
 
 export default function Main() {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositorios, setRepositorios] = useState([]);
+
+  function handleInputChange({ target }) {
+    setNewRepo(target.value);
+  }
+
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+    async function submit() {
+      const response = await api.get(`/repos/${newRepo}`);      
+      const data = {
+        name: response.data.full_name,
+      };
+      setRepositorios([...repositorios, data]);
+      setNewRepo("");
+    }
+    console.log(repositorios);
+    submit();
+  }, [repositorios, newRepo]);
+
   return (
     <Container>
       <h1>
         <FaGithub size={25} />
         Meus repositórios
       </h1>
-      <Form onSubmit={() => {}}>
-        <input type="text" />
+      <Form onSubmit={handleSubmit}>
+        <input type="text" value={newRepo} onChange={handleInputChange} />
+
         <SubmitButton>
           <FaPlus color="#fff" size={14} />
         </SubmitButton>
